f1901a83cfe417a0ac6e6ec203467002aa3f2a5c
Helena Flausino <helenaflausino@gmail.com>
2026-03-17 09:19:59 -0300

    learn(ap09-git): Validando datas

--- ap09_estrutura_de_selecao/script.sql
+++ ap09_estrutura_de_selecao/script.sql
@@ -1,20 +1,69 @@
 DO $$
-DECLARE
-    valor INT := fn_valor_aleatorio_entre(1,12);
-    mensagem VARCHAR(200);
+DECLARE 
+    data INT := 06232323;
+    dia INT;
+    mes INT;
+    ano INT;
+    data_valida BOOL := TRUE;
 BEGIN
-    RAISE NOTICE 'Valor gerado: %', valor;
-    CASE valor
-        WHEN  1, 3, 5, 7, 9 THEN
-            mensagem := 'ímpar';
-        WHEN 2, 4, 6, 8, 10 THEN
-            mensagem := 'par';
-        ELSE 
-            mensagem := 'fora do intervalo';
-    END CASE;
-    RAISE NOTICE '%', mensagem;
+    dia := data/100000;
+    mes := (data % 1000000) / 10000;
+    ano := data % 10000;
+    RAISE NOTICE 'A data é %/%/%', dia, mes, ano;
+    IF ano >= 1 THEN
+        CASE 
+            WHEN mes > 12 OR mes < 1 OR dia < 1 OR dia > 31 THEN 
+                data_valida := FALSE;
+
+        ELSE
+            IF ((mes = 4 OR mes = 6 OR mes = 9 OR mes = 11) AND dia> 30) THEN
+                data_valida := FALSE;
+            ELSE
+                IF mes = 2 THEN
+                    CASE
+                        WHEN ((ano % 4 = 0) AND (ano % 400 != 0)) THEN
+                            IF dia > 29 THEN
+                                data_valida := FALSE;
+                            END IF;
+                        ELSE
+                            IF dia > 28 THEN
+                                data_valida := FALSE
+                            END IF;
+                    END CASE;
+                END IF;
+            END IF;
+        END CASE;
+    ELSE 
+        data_valida := FALSE;
+    END IF;
+    CASE
+        WHEN data_valida THEN
+            RAISE NOTICE 'A data % é válida', data_valida;
+        ELSE
+            RAISE NOTICE 'A data % é inválida', data_valida;
+    END CASE
 END;
 $$
+
+--daqui para cima é 17/03/26
+
+-- DO $$
+-- DECLARE
+--     valor INT := fn_valor_aleatorio_entre(1,12);
+--     mensagem VARCHAR(200);
+-- BEGIN
+--     RAISE NOTICE 'Valor gerado: %', valor;
+--     CASE valor
+--         WHEN  1, 3, 5, 7, 9 THEN
+--             mensagem := 'ímpar';
+--         WHEN 2, 4, 6, 8, 10 THEN
+--             mensagem := 'par';
+--         ELSE 
+--             mensagem := 'fora do intervalo';
+--     END CASE;
+--     RAISE NOTICE '%', mensagem;
+-- END;
+-- $$
 
 -- DO $$
 -- DECLARE
